e64a2d2f2f874ba9bf395dcd846624c0dd79379c
Adefeso Qudus Adebola || Qdus <adefesoq@gmail.com>
2023-05-01 23:34:10 +0100

    feat: add a graient border to post card

--- app/page.tsx
+++ app/page.tsx
@@ -2,31 +2,56 @@ import Link from "next/link";
 import React from "react";
 import getPostMetadata from "@/lib/getposts";
 import { montserrat } from "@/utils";
+import { ArrowSmallRightIcon } from "@heroicons/react/24/solid";
+import { CSSProperties } from "react";
 
 const Home = () => {
   return (
     <div>
-      <div className="p-4 max-w-[1000px] mx-auto">
+      <div className="mx-auto max-w-[1000px] p-4">
         <div className="mb-5">
-          <p className="text-[#fcfcfc] uppercase font-semibold">
+          <p className="font-semibold uppercase text-[#fcfcfc]">
             Recently Published
           </p>
         </div>
-        <div className="rounded-md shadow-sm space-y-4 lg:grid lg:grid-cols-2 lg:space-y-0 lg:gap-5">
+        <div className="space-y-4 rounded-md shadow-sm lg:grid lg:grid-cols-2 lg:gap-5 lg:space-y-0">
           {getPostMetadata().map((post, i) => {
             return (
-              <div key={i} className="bg-[#353a35] ">
+              <div
+                key={i}
+                style={
+                  {
+                    "--dark-purple": "53 58 53",
+                    "--light-purple": "249 248 113",
+
+                    "--bg-color":
+                      "linear-gradient(rgb(var(--dark-purple)), rgb(var(--dark-purple)))",
+                    "--border-color": `linear-gradient(145deg,
+            rgb(var(--light-purple)) 0%,
+            rgb(var(--light-purple) / 0.3) 33.33%,
+            rgb(var(--light-purple) / 0.14) 66.67%,
+            rgb(var(--light-purple) / 0.1) 100%)
+          `,
+                  } as CSSProperties
+                }
+                className="post-card-wrapper rounded-lg border border-transparent [background:padding-box_var(--bg-color),border-box_var(--border-color)]"
+              >
                 <Link
                   href={`/posts/${post.slug}`}
-                  className="post-card p-4 rounded block"
+                  className="post-card block p-4"
                 >
                   <h2
-                    className={`${montserrat.variable} font-montserrat text-2xl text-[#E9C7A5] mb-2 font-semibold`}
+                    className={`${montserrat.variable} mb-2 font-montserrat text-2xl font-semibold text-[#E9C7A5]`}
                   >
                     {post.title}
                   </h2>
-                  <p className="text-[#fcfcfc] text-sm mb-3">{post.subtitle}</p>
-                  <button className="text-[#C4D46C]">read more</button>
+                  <p className="mb-3 text-sm text-[#fcfcfc]">{post.subtitle}</p>
+                  <button className="flex items-center text-[#C4D46C]">
+                    read more{" "}
+                    <span>
+                      <ArrowSmallRightIcon className="ml-3 h-5 w-5 text-[#C4D46C]" />
+                    </span>
+                  </button>
                 </Link>
               </div>
             );
