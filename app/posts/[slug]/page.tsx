f1d6a0a919773e983cc0da15955325e3da600803
Adefeso Qudus Adebola || Qdus <adefesoq@gmail.com>
2023-05-20 11:59:11 +0100

    chore: `dynamic toc`

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -71,11 +71,21 @@ const PostPage = async ({ params }: PageProps) => {
         date={publishDate}
         tags={postTags}
       />
-      <div className="prose max-w-none p-5 prose-h1:text-[rgb(233,199,165)] prose-h2:text-[#E9C7A5] prose-h3:text-[#E9C7A5] prose-h4:text-[#E9C7A5] prose-p:text-white prose-a:text-white prose-strong:text-white  prose-li:text-white prose-th:text-white prose-td:text-white prose-code:text-white">
-        <article className="prose mx-auto max-w-3xl">
+      <div className="relative lg:flex lg:w-[80%] lg:ml-auto lg:items-start lg:gap-16 prose max-w-none p-5 prose-p:leading-[1.4]  prose-h2:text-[#E9C7A5] prose-h3:text-[#E9C7A5] prose-h4:text-[#E9C7A5] prose-p:text-white prose-a:text-white prose-strong:text-white  prose-li:text-white prose-th:text-white prose-td:text-white prose-code:text-white">
+        <article className="prose mx-auto max-w-[800px] prose-h1:text-[rgb(233,199,165)]">
           {/* @ts-expect-error Server Component*/}
           <MDXRemote source={post.content} options={options} />
         </article>
+        <div
+          className="hidden lg:block lg:w-[400px] lg:sticky lg:top-10 lg:right-0 prose-h1:text-white prose-p:leading-[.8] prose-p:text-gray-200 prose-p:text-sm"
+          style={{ borderLeft: "1px solid gray", paddingLeft: "1rem" }}
+        >
+          <h1 className="font-medium text-base capitalize mb-3">
+            On this page
+          </h1>
+          <p>Setup</p>
+          <p>Folder Structure</p>
+        </div>
       </div>
     </main>
   );
